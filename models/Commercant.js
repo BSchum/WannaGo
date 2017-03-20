3c8b103421816413236cfcad21d1cef59df47e4e
brice schumacher <brice schumacher>
2017-03-20 09:32:14 +0100

    Add models

--- models/Commercant.js
+++ models/Commercant.js
@@ -4,16 +4,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-<<<<<<< HEAD
-var commercantSchema = new Schema({
-    profile : {type: Schema.Types.ObjectId, ref: 'User'},
-    commerce : [{type : Schema.Types.ObjectId , ref:'Commerce'}]
-=======
 var Schema = new Schema({
     commerce : {type: Schema.Types.ObjectId, ref: 'User'},
     post : [{type : Schema.Types.ObjectId , ref:'Post'}],
     reservation : [{type: Schema.Types.ObjectId, ref:'Reservation'}]
->>>>>>> 0bd1581c5bb115c23314310c82d71d950f0d1cef
 });
 
 module.exports = mongoose.model('Voyageur', voyageurSchema);
